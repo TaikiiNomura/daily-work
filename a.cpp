696027b715e658f2d110490a285d0b32f594bfa1
TaikiiNomura <208572257+TaikiiNomura@users.noreply.github.com>
2026-01-03 00:04:59 +0900

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+#define rep(i,N) for(int i=0;i<(N);i++)
+#define Yes {cout<<"Yes\n";return 0;}
+#define No {cout<<"No\n";return 0;}
+//
+int main()
+{
+    cout<<"Hello"<<endl;
+}
