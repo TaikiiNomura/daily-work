051ebb4629473eb1d9ade8b6848a36a230f326a6
TaikiiNomura <23b2119@sit.shonan-it.ac.jp>
2026-03-07 03:29:20 +0900

    Update abc410b.cpp

--- src/abc410b.cpp
+++ src/abc410b.cpp
@@ -1,37 +1,28 @@
-#include<algorithm>
 #include<iostream>
-#include<vector>
 using namespace std;
-#define rep(i,N) for(int i=0;i<N;i++)
-#define rng(x) x.begin(), x.end()
-#define printv(v) rep(i,v.size())cout<<v[i]<<(i==v.size()-1?'\n':' ');
-#define Yes {cout<<"Yes\n";return 0;}
-#define No {cout<<"No\n";return 0;}
+int n,q,b[110];
 int main()
 {
-    int N,Q; cin>>N>>Q;
-    vector<int> B(N,0);
-    
-    rep(i,Q)
+  cin>>n>>q;
+  for(int i=0;i<q;i++)
+  {
+    int x;cin>>x;
+    if(x==0)
     {
-        int X; cin>>X;
-        if(X>=1)
-        {
-            B[X-1]++;
-            cout<<X;
-        }
-        else
-        {
-            int min_val=*min_element(rng(B));
-            rep(j,N)if(B[j]==min_val)
-            {
-                cout<<j+1;
-                B[j]++;
-                break;
-            }
-        }
-        if(i==(Q-1))cout<<endl;
-        else cout<<' ';
+      int cnt_min=10000;
+      for(int j=0;j<n;j++)cnt_min=min(b[j],cnt_min);
+      for(int j=0;j<n;j++)if(b[j]==cnt_min)
+      {
+        b[j]++;
+        cout<<j+1;
+        break;
+      }
     }
-    // printv(B);
+    else
+    {
+      b[x-1]++;
+      cout<<x;
+    }
+    cout<<(i==q-1?'\n':' ');
+  }
 }
