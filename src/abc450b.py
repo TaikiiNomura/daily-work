273f28cf56b19be2ed71c14716d8072f77185b4e
TaikiiNomura <23b2119@sit.shonan-it.ac.jp>
2026-03-23 22:59:50 +0900

    Update abc450b.py

--- src/abc450b.py
+++ src/abc450b.py
@@ -1,12 +1,9 @@
 n=int(input())
 l=[list(map(int,input().split())) for _ in range(n-1)]
+ans="No"
 for a in range(n-2):
   for b in range(a+1,n-1):
     for c in range(b+1,n):
-      abc=l[a][b-1-a]+l[b][c-1-b]
-      ac=l[a][c-1-a]
-      # print(a,b,c,abc,ac)
-      if abc<ac:
-        print("Yes")
-        exit()
-print("No")
+      if l[a][b-1-a]+l[b][c-1-b]<l[a][c-1-a]:
+        ans="Yes"
+print(ans)
